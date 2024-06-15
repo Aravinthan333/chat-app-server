4feebc16dad1372fbb7f077cb07a88f0e3a1ff35
Aravinthan333 <aravinthan2606@gmail.com>
2024-06-15 22:52:52 +0530

    changed cors

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const { app, server } = require("./socket/index");
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    // origin: process.env.FRONTEND_URL,
+    origin: "*",
     credentials: true,
   })
 );
