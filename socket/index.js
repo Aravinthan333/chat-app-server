cd749ff223f2565c4a7b2f5605cf4e1122d541bc
Aravinthan333 <aravinthan2606@gmail.com>
2024-06-25 10:38:54 +0530

    ...

--- socket/index.js
+++ socket/index.js
@@ -17,7 +17,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     // origin: process.env.FRONTEND_URL,
-    origin: "https://social-chat-app-001.netlify.app/",
+    origin: "",
     credentials: true,
   },
 });
