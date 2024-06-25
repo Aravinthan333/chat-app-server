6ff91e80c29a1be1b8ac9d0c8ab3b24ccc5a5950
Aravinthan333 <aravinthan2606@gmail.com>
2024-06-25 10:47:52 +0530

    .....

--- index.js
+++ index.js
@@ -8,13 +8,15 @@ const { app, server } = require("./socket/index");
 
 // const app = express();
 
-app.use(
-  cors({
-    // origin: process.env.FRONTEND_URL,
-    origin: "https://social-chat-app-001.netlify.app/",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     // origin: process.env.FRONTEND_URL,
+//     origin: "*",
+//     credentials: true,
+//   })
+// );
+
+app.use(cors);
 
 app.use(express.json());
 app.use(cookiesParser());
